cb6fe75fa76246d896311e80fba48aaf77b4d6d8
Tiark Rompf <tiark.rompf@epfl.ch>
2013-07-05 08:10:07 +0200

    small fixes

--- src/test/scala/lms/tutorial/shonan.scala
+++ src/test/scala/lms/tutorial/shonan.scala
@@ -3,6 +3,8 @@ package scala.lms.tutorial
 import scala.virtualization.lms.common._
 import scala.reflect.SourceContext
 
+// sbt ~test-only *Shonan*
+
 class ShonanTest extends TutorialFunSuite {
   val under = "dslapi"
 
@@ -59,6 +61,8 @@ class ShonanTest extends TutorialFunSuite {
   1) add compile snippet
   2) add conditional
   3) stage mv prod
+      - staticData(a)
+      - NewArray[Int](n)     
   4) Range vs Rep[Range]
   5) unrollIf
 */
@@ -111,7 +115,7 @@ class ShonanTest extends TutorialFunSuite {
 
 /* STEP 0.5: static vs dynamic conditional
 
-  now we're adding some basic codgen facilities and play
+  now we're adding some basic codegen facilities and play
   with static vs dynamic expressions. fully static expressions
   do not show up in generated code. if we change the 
   condition to depend on the (dynamic) input array, an if/else
