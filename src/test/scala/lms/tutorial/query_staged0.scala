3aa0c3b9ce74f57f412e169c2993537386136952
Nada Amin <namin@alum.mit.edu>
2014-09-03 00:15:07 +0200

    simple version

---
+++ src/test/scala/lms/tutorial/query_staged0.scala
@@ -0,0 +1,71 @@
+package scala.lms.tutorial
+
+import scala.virtualization.lms.common._
+
+object query_staged0 {
+trait QueryCompiler extends Dsl with QueryProcessor
+with ScannerBase {
+  override type Schema = Vector[String]
+  def Schema(schema: String*): Schema = schema.toVector
+
+  type Table = Rep[String] // dynamic filename
+  def tableFor(tableName: String): Table = unit(defaultFilenameFor(tableName))
+
+  // low-level processing
+  type RField = Rep[String]
+  type Fields = Vector[RField]
+
+  case class Record(fields: Fields, schema: Schema) {
+    def apply(key: String): RField = fields(schema indexOf key)
+    def apply(keys: Schema): Fields = keys.map(this apply _)
+  }
+
+  def processCSV(filename: Rep[String], schema: Schema, fieldDelimiter: Char, externalSchema: Boolean)(yld: Record => Rep[Unit]): Rep[Unit] = {
+    val s = newScanner(filename)
+    val last = schema.last
+    def nextRecord = Record(schema.map{x => s.next(if (x==last) '\n' else fieldDelimiter)}, schema)
+    if (!externalSchema) {
+      // the right thing would be to dynamically re-check the schema,
+      // but it clutters the generated code
+      // schema.foreach(f => if (s.next != f) println("ERROR: schema mismatch"))
+      nextRecord // ignore csv header
+    }
+    while (s.hasNext) yld(nextRecord)
+    s.close
+  }
+
+  def printSchema(schema: Schema) = println(schema.mkString(defaultFieldDelimiter.toString))
+
+  def printFields(fields: Fields) = printf(fields.map{_ => "%s"}.mkString("\"", defaultFieldDelimiter.toString, "\\n\""), fields: _*)
+
+  def evalPred(p: Predicate)(rec: Record): Rep[Boolean] = p match {
+    case Eq(a1, a2) => evalRef(a1)(rec) == evalRef(a2)(rec)
+  }
+
+  def evalRef(r: Ref)(rec: Record): Rep[Any] = r match {
+    case Field(name) => rec(name)
+    case Value(x) => x
+  }
+
+  def resultSchema(o: Operator): Schema = o match {
+    case Scan(_, schema, _, _)   => schema
+    case Filter(pred, parent)    => resultSchema(parent)
+    case Project(schema, _, _)   => schema
+    case PrintCSV(parent)        => Schema()
+  }
+
+  def execOp(o: Operator)(yld: Record => Rep[Unit]): Rep[Unit] = o match {
+    case Scan(filename, schema, fieldDelimiter, externalSchema) =>
+      processCSV(tableFor(filename), schema, fieldDelimiter, externalSchema)(yld)
+    case Filter(pred, parent) =>
+      execOp(parent) { rec => if (evalPred(pred)(rec)) yld(rec) }
+    case Project(newSchema, parentSchema, parent) =>
+      execOp(parent) { rec => yld(Record(rec(parentSchema), newSchema)) }
+    case PrintCSV(parent) =>
+      val schema = resultSchema(parent)
+      printSchema(schema)
+      execOp(parent) { rec => printFields(rec.fields) }
+  }
+  def execQuery(q: Operator): Rep[Unit] = execOp(q) { _ => }
+}
+}
