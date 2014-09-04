0cd15aed3c83259444a13264395343a468dda335
Nada Amin <namin@alum.mit.edu>
2014-09-05 01:21:57 +0200

    add hand.scala version, not that different superficially, can skip lines a bit faster

---
+++ src/out/query_t1gram2.hand.scala
@@ -0,0 +1,18 @@
+class Snippet extends (String=>Unit) {
+  def apply(fn: String): Unit = {
+    println("Phrase,Year,MatchCount,VolumeCount")
+    val s = new scala.lms.tutorial.Scanner(fn)
+    while (s.hasNext) {
+      val phrase = s.next('\t')
+      if (phrase == "Auswanderung") {
+        val year = s.next('\t')
+        val matchCount = s.next('\t')
+        val volumeCount = s.next('\n')
+        println(s"$phrase,$year,$matchCount,$volumeCount")
+      } else {
+        s.next('\n')
+      }
+    }
+    s.close
+  }
+}
