16b6ed5c9a659be779ec51d9b9c4e4c4166e7a31
Tiark Rompf <tiark.rompf@epfl.ch>
2014-09-05 00:42:45 +0200

    group by syntax, not 100% SQL but close enough

--- src/test/scala/lms/tutorial/query.scala
+++ src/test/scala/lms/tutorial/query.scala
@@ -32,13 +32,14 @@ trait SQLParser extends QueryAST {
         val (fs,fs1) = fs2s.map { case a~b => (b.getOrElse(a),a) }.unzip
         (Schema(fs:_*),Schema(fs1:_*))
       }
+    def fieldList1:  Parser[Schema] = repsep(fieldIdent,",") ^^ (fs => Schema(fs:_*))
 
     def predicate: Parser[Predicate] = ref ~ "=" ~ ref ^^ { case a ~ _ ~ b => Eq(a,b) }
     def ref: Parser[Ref] = fieldIdent ^^ Field | """'\w*'""".r ^^ (s => Value(s.drop(1).dropRight(1))) |       
           """[0-9]+""".r ^^ (s => Value(s.toInt))
     def tableClause: Parser[Operator] = 
-      tableIdent ~ opt("schema" ~> fieldList) ~ opt("delim" ~> ("""\t""" ^^ (_ => '\t') | """.""".r ^^ (_.head))) ^^ { 
-        case table ~ schema ~ delim => Scan(table, schema.map(_._1), delim)
+      tableIdent ~ opt("schema" ~> fieldList1) ~ opt("delim" ~> ("""\t""" ^^ (_ => '\t') | """.""".r ^^ (_.head))) ^^ { 
+        case table ~ schema ~ delim => Scan(table, schema, delim)
       } |
       ("(" ~> stm <~ ")")
     def fromClause: Parser[Operator] = 
@@ -46,13 +47,18 @@ trait SQLParser extends QueryAST {
     def joinClause: Parser[Operator] = 
       ("nestedloops" ~> repsep(tableClause, "join") ^^ { _.reduceLeft((a,b) => Join(a,b)) }) |
       (repsep(tableClause, "join") ^^ { _.reduceLeft((a,b) => HashJoin(a,b)) })
-      
+
     def selectClause: Parser[Operator=>Operator] = 
       "select" ~> ("*" ^^ { _ => (op:Operator) => op } | fieldList ^^ { case (fs,fs1) => Project(fs,fs1,_:Operator) })
     def whereClause: Parser[Operator=>Operator] = 
       opt("where" ~> predicate ^^ { p => Filter(p, _:Operator) }) ^^ { _.getOrElse(op => op)}
 
-    def stm: Parser[Operator] = selectClause ~ fromClause ~ whereClause ^^ { case p ~ s ~ f => p(f(s)) }
+    def groupClause: Parser[Operator=>Operator] = 
+      opt("group" ~> "by" ~> fieldList1 ~ ("sum" ~> fieldList1) ^^ { case p1 ~ p2 => Group(p1,p2, _:Operator) }) ^^ { _.getOrElse(op => op)}
+
+    def stm: Parser[Operator] = selectClause ~ fromClause ~ whereClause ~ groupClause ^^ { 
+      case p ~ s ~ f ~ g => g(p(f(s)))
+    }
   }
   def parseSql(input: String) = Grammar.parseAll(Grammar.stm, input).get // cleaner error reporting?
 }
@@ -275,9 +281,9 @@ class QueryTest extends TutorialFunSuite {
   testquery("t3", "select Name from t.csv where Flag='yes'")
   testquery("t4", "select * from nestedloops t.csv join (select Name as Name1 from t.csv)")
   testquery("t5", "select * from nestedloops t.csv join (select Name from t.csv)")
-  testquery("t4h", "select * from t.csv join (select Name as Name1 from t.csv)")
-  testquery("t5h", "select * from t.csv join (select Name from t.csv)")
-  testquery("t6")
+  testquery("t4h","select * from t.csv join (select Name as Name1 from t.csv)")
+  testquery("t5h","select * from t.csv join (select Name from t.csv)")
+  testquery("t6", "select * from t.csv group by Name sum Value") // not 100% right syntax, but hey ...
 
   val defaultEvalTable = dataFilePath("t1gram.csv")
   testquery("t1gram1", "select * from ? schema Phrase, Year, MatchCount, VolumeCount delim \\t")
