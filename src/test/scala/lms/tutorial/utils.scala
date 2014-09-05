a26795d747b1cf614132b1bf62e81214cbf71751
Tiark Rompf <tiark.rompf@epfl.ch>
2014-09-05 10:34:45 +0200

    fix overwrite

--- src/test/scala/lms/tutorial/utils.scala
+++ src/test/scala/lms/tutorial/utils.scala
@@ -8,7 +8,7 @@ trait LibSuite extends FunSuite {
 }
 
 trait TutorialFunSuite extends LibSuite {
-  val overwriteCheckFiles = true // should be false; temporary set to true only to simplify development
+  val overwriteCheckFiles = false // should be false; temporary set to true only to simplify development
 
   val prefix = "src/out/"
   val under: String
