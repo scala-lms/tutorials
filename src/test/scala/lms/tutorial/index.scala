d7e3f4802338cc0ef7b1da05b66afdfa4063d68b
Tiark Rompf <tiark.rompf@epfl.ch>
2014-09-18 23:48:39 -0500

    caveat emptor

--- src/test/scala/lms/tutorial/index.scala
+++ src/test/scala/lms/tutorial/index.scala
@@ -147,6 +147,9 @@ Advanced:
 LMS In Depth
 ------------
 
+The material below has been extracted from Tiark's PhD thesis ([PDF](http://lampwww.epfl.ch/~rompf/thesis_120716.pdf)) and is still undergoing 
+revision for the web.
+
 1. [About generative programming](01_overview.html)
 1. [Generative programming and LMS basics](02_basics.html)
 1. [Intermediate representation and optimizations](03_compiler.html)
