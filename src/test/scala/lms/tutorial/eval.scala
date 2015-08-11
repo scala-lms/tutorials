3145f14befd1dfdff5b288a64ad5b795bf289392
Nada Amin <namin@alum.mit.edu>
2015-08-11 19:23:55 +0200

    commentary

--- src/test/scala/lms/tutorial/eval.scala
+++ src/test/scala/lms/tutorial/eval.scala
@@ -1,29 +1,97 @@
+/**
+An auto-specializing interpreter
+================================
+
+We show an end-to-end LMS example, exploiting once more that a "staged
+interpreter is a compiler" but with a twist: parametric staging!
+
+- Georg Ofenbeck et al. at GPCE 2013
+  _Spiral in Scala: Towards the Systematic Construction of Generators for Performance Libraries_
+  The technique of abstracting over staging decision is inspired by this work.
+
+- Kenichi Asai at GPCE 2014
+  _Compiling a Reflective Language using MetaOCaml_
+  Specializing lambdas in our interpreter is loosely inspired by this
+  paper, which features a much more powerful and flexible Scheme-like
+  object language.
+
+*/
+
 package scala.lms.tutorial
 
+/**
+Our Base Evaluator
+------------------
+
+We use an object `eval` to which we can easily refer in the generated code.
+
+*/
 object eval {
+
+/**
+
+We use plain Scala ASTs for our `Term`s and `Value`s. We change the
+`toString` representation of the ASTs so that each is self-evaluating in
+Scala, so that they can be printed as such in the generated code.
+
+*/
   sealed trait Term
   type Env = Map[String, Value]
+/**
+We see our first `R[_]` to abstract over whether we have a `Rep` (staged) value or a `NoRep` (unstaged) one.
+*/
   type Cont[R[_]] = R[Value] => R[Value]
   sealed trait Value
+/**
+Our object language just has enough features to execute fibonacci.
+Hence, the terms include just the following:
+*/
+  // number
   case class I(n: Int) extends Value with Term
+  // boolean
   case class B(b: Boolean) extends Value with Term
+  // variable
   case class V(s: String) extends Term {
     override def toString = "V(\""+s+"\")"
   }
+  // primitive
   case class P(p: String) extends Value with Term {
     override def toString = "P(\""+p+"\")"
   }
+  // lambda
   case class L(compile: Boolean, param: String, body: Term) extends Term {
     override def toString = "L("+compile+", \""+param+"\", "+body+")"
   }
+  // application
   case class A(fun: Term, args: List[Term]) extends Term
+  // if-expression
   case class If(c: Term, thenp: Term, elsep: Term) extends Term
+/**
+Now, on to non-trivial values.
+
+A `Closure` represents the value of uncompiled lambda. We leave the quoting of closures to the
+code generation because we need to quote the environment.
+*/
   case class Clo(param: String, body: Term, env: Map[String, Value]) extends Value
+/**
+An `Evalfun` represents the value of a compiled lambda, that is a function `Value => Value`.
+However, in order for these to self-quote nicely, we unfortunately need a level of 
+indirection from a key (a `String`) to be mapped to the actual compiled function
+below.
+*/
   case class Evalfun(key: String) extends Value {
     override def toString = "Evalfun(\""+key+"\")"
   }
+/**
+Finally, we sometimes need a `Rep[Value]` for the parameter of a lambda to be compiled.
+A `Code` enables such a representation.
+*/
   case class Code[R[_]](c: R[Value]) extends Value
 
+/**
+Our map from the key of each `Evalfun` to the actual compiled function.
+This is a bit of a leak, and so we need to reset the map between runs.
+*/
   var funs = Map[String, Value => Value]()
   def addFun(f: Value => Value): String = {
     val key = "f"+funs.size
@@ -35,12 +103,18 @@ object eval {
   }
   def evalfun(f: Value => Value) = Evalfun(addFun(f))
 
+/**
+Applying a primitive is straightforward.
+*/
   def apply_primitive(p: String, args: List[Value]): Value = (p, args) match {
     case ("<", List(I(a), I(b))) => B(a < b)
     case ("+", List(I(a), I(b))) => I(a+b)
     case ("-", List(I(a), I(b))) => I(a-b)
   }
 
+/**
+The `Ops` typeclass encapsulates all we need to know in order to parametrically stage the code. We define `Ops[Rep]` later outside the object.
+*/
   trait Ops[R[_]] {
     def lift(v: Value): R[Value]
     def base_apply(fun: R[Value], args: List[R[Value]], env: Env, cont: Cont[R]): R[Value]
@@ -49,6 +123,9 @@ object eval {
     def makeFun(f: R[Value] => R[Value]): R[Value]
     def inRep: Boolean
   }
+/**
+The type `NoRep` enables us to run the evaluator "now".
+*/
   type NoRep[A] = A
   implicit object OpsNoRep extends Ops[NoRep] {
     def lift(v: Value) = v
@@ -60,6 +137,9 @@ object eval {
     def inRep = false
   }
 
+/**
+The unstaged base application will also be called from the generated code.
+*/
   def base_apply_norep(fun: Value, args: List[Value], env: Env, cont: Cont[NoRep]) = fun match {
     case Clo(param, body, cenv) =>
       base_eval[NoRep](body, cenv + (param -> args(0)), cont)
@@ -70,6 +150,11 @@ object eval {
       cont(apply_primitive(p, args))
   }
 
+/**
+Now, on to our evaluation functions.
+
+Evaluate a list of terms.
+*/
   def base_evlist[R[_]:Ops](exps: List[Term], env: Env, cont: List[R[Value]] => R[Value]): R[Value] = exps match {
     case Nil => cont(Nil)
     case e::es => base_eval[R](e, env, { v =>
@@ -79,10 +164,23 @@ object eval {
     })
   }
 
+/**
+Evaluate a top-level term.
+*/
   def top_eval[R[_]:Ops](exp: Term): R[Value] = {
     reset()
     base_eval[R](exp, Map(), x => x)
   }
+/**
+Finally, the evaluator.
+
+A key take-away is that by just staging the continuation
+from a `Value => Value` to a `Rep[Value] => Rep[Value]`,
+we can generate code for a known term instead of evaluating it directly.
+
+However, the cost of combining the staged and unstaged `base_eval` is
+that the function is no longer tail-recursive.
+*/
   def base_eval[R[_]:Ops](exp: Term, env: Env, cont: Cont[R]): R[Value] = {
     val o = implicitly[Ops[R]]; import o._
     exp match {
@@ -124,6 +222,10 @@ object eval {
   }
 }
 
+/**
+The LMS bits
+------------
+*/
 import eval._
 import scala.lms.common._
 
@@ -196,6 +298,10 @@ abstract class EvalDslDriver extends EvalDsl with EvalDslImpl with CompileScala 
   }
 }
 
+/**
+The Tests
+---------
+*/
 class TestEvaluator extends TutorialFunSuite {
   val under = "eval_"
 
@@ -214,24 +320,29 @@ class TestEvaluator extends TutorialFunSuite {
   def sumf(c: Boolean) = L(c, "f", L(c, "sumf", L(c, "n", If(A(P("<"), List(V("n"), I(0))), I(0), A(P("+"), List(A(V("f"), List(V("n"))), A(V("sumf"), List(A(P("-"), List(V("n"), I(1)))))))))))
 
   test ("fib 7 evaluated") {
-    assertResult(I(13)){top_eval[NoRep](A(A(Y(false), List(fib(false))), List(I(7))))}
+    assertResult(I(13)){
+      top_eval[NoRep](A(A(Y(false), List(fib(false))), List(I(7))))}
   }
 
   test ("fib 7 compiled fib") {
     checkOut("yfibc", "scala",
-      assertResult(I(13)){top_eval[NoRep](A(A(Y(false), List(fib(true))), List(I(7))))})
+      assertResult(I(13)){
+        top_eval[NoRep](A(A(Y(false), List(fib(true))), List(I(7))))})
   }
 
   test ("fib 7 compiled all") {
     checkOut("ycfibc", "scala",
-      assertResult(I(13)){top_eval[NoRep](A(A(Y(true), List(fib(true))), List(I(7))))})
+      assertResult(I(13)){
+        top_eval[NoRep](A(A(Y(true), List(fib(true))), List(I(7))))})
   }
 
   test ("sum of fibs evaluated") {
-    assertResult(I(33)){top_eval[NoRep](A(A(Y(false), List(A(sumf(false), List(A(Y(false), List(fib(false))))))), List(I(7))))}
+    assertResult(I(33)){
+      top_eval[NoRep](A(A(Y(false), List(A(sumf(false), List(A(Y(false), List(fib(false))))))), List(I(7))))}
   }
 
   test ("sum of fibs compiled") {
-    assertResult(I(33)){top_eval[NoRep](A(A(Y(true), List(A(sumf(true), List(A(Y(true), List(fib(true))))))), List(I(7))))}
+    assertResult(I(33)){
+      top_eval[NoRep](A(A(Y(true), List(A(sumf(true), List(A(Y(true), List(fib(true))))))), List(I(7))))}
   }
 }
