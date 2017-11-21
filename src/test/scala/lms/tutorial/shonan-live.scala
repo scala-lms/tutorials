3cf397b5b29aefc1d0f64a23afb2e13b78b2cfe1
Nada Amin <namin@alum.mit.edu>
2017-11-21 00:11:05 +0000

    revert done shonan live committed by mistake in fef1f99

--- src/test/scala/lms/tutorial/shonan-live.scala
+++ src/test/scala/lms/tutorial/shonan-live.scala
@@ -20,41 +20,40 @@ class ShonanLiveTest extends TutorialFunSuite {
         A(0, 0, 0, 0, 0),
         A(0, 0, 1, 0, 1))
 
+    def matrix_vector_prod(a: Array[Array[Int]], v: Array[Int]) = {
+      val n = a.length
+      val v1 = new Array[Int](n)
+
+      for (i <- (0 until n): Range) {
+        for (j <- (0 until n)) {
+          v1(i) = v1(i) + a(i).apply(j) * v(j)
+        }
+      }
+      v1
+    }
+
+    // let's run it on some static input:
+    val v = A(1,1,1,1,1)
+    val v1 = matrix_vector_prod(a, v)
+    
+    val result = v1.mkString(",")
+    check("shonan-hmm-live", result)
+  }
+
+
+
+
+  /*
     val snippet = new LMS_Driver[Array[Int],Array[Int]] {
-      def unrollIf(c: Boolean)(r: Range) = new {
-        def foreach(f: Rep[Int] => Rep[Unit]): Rep[Unit] = {
-          if (c) {
-            for (j <- r) {
-              f(j)
-            }
-          } else {
-            for (j <- (r.start until r.end):Rep[Range]) {
-              f(j)
-            }
-          }
-        }
-      }
-
-      def matrix_vector_prod(a0: Array[Array[Int]], v: Rep[Array[Int]]) = {
-        val n = a0.length
-        val a = staticData(a0)
-        val v1 = NewArray[Int](n)
-
-        for (i <- (0 until n): Range) {
-          val sparse = a0(i).count(_ != 0) < 3
-          for (j <- unrollIf(sparse)(0 until n)) {
-            v1(i) = v1(i) + a(i).apply(j) * v(j)
-          }
-        }
-        v1
-      }
-
       def snippet(v: Rep[Array[Int]]) = {
-        matrix_vector_prod(a, v)
+        println("hello")
+        v
       }
     }
     exec("shonan-hmm-live", snippet.code)
-  }
+  */
+
+
 
 /*
   DEMO:
