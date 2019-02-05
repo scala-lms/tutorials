1f2ca0dcdcbc4b7f5ae0811ea7bfbe4fae1e5c76
Tiark Rompf <tiark.rompf@gmail.com>
2019-02-04 22:47:29 -0500

    dynvar test passes!

--- src/out/dynvar0.check.scala
+++ src/out/dynvar0.check.scala
@@ -1,20 +1,10 @@
 /*****************************************
 Emitting Generated Code
 *******************************************/
-class Snippet extends ((Int)=>(Int)) {
-  def apply(x0:Int): Int = {
+class Snippet extends (Int => Int){
+  def apply(x0: Int): Int = {
     val x1 = x0 == 0
-    val x3 = if (x1) {
-      val x2 = if (x1) {
-        0
-      } else {
-        2
-      }
-      x2
-    } else {
-      1
-    }
-    x3
+    if (x1) if (x1) 0 else 2 else 1
   }
 }
 /*****************************************
--- src/out/dynvar1.check.scala
+++ src/out/dynvar1.check.scala
@@ -1,15 +1,9 @@
 /*****************************************
 Emitting Generated Code
 *******************************************/
-class Snippet extends ((Int)=>(Int)) {
-  def apply(x0:Int): Int = {
-    val x1 = x0 == 0
-    val x2 = if (x1) {
-      0
-    } else {
-      1
-    }
-    x2
+class Snippet extends (Int => Int){
+  def apply(x0: Int): Int = {
+    if (x0 == 0) 0 else 1
   }
 }
 /*****************************************
